a7127f368c775d8e766a6d9827d1f67acd6f394f
the6thm0nth <hoonch3@gmail.com>
2018-01-09 10:48:33 +0900

    add app.js for running by node

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  project: 'react-flux-boilerplate',
+  port: 8080,
+  api: {
+    host: 'http://localhost',
+    port: 18080,
+    prefix: 'api'
+  },
+  ssl: {
+    pfx: '',
+    passphrase: ''
+  }
+}
